87930e0ba0208d0d87dcf0ca853b497e73102f7a
hanguoyao <hanguoyao@qq.com>
2019-10-21 17:31:18 +0800

    aaaaa

--- src/main/java/com/cmos/controller/MyContest.java
+++ src/main/java/com/cmos/controller/MyContest.java
@@ -3,6 +3,6 @@ package com.cmos.controller;
 public class MyContest {
 	//ÊµÀı´úÂë
 	public static void main(String args) {
-		System.out.println("111111111111");
+		System.out.println("111111111111@@@@@@@@@@@@");
 	}
 }
